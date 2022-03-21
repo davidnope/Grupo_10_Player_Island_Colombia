ba0e7d9e0226401872a0a0b5e94c6c978978daca
davidnope <dnope745@gmail.com>
2022-03-21 17:58:43 -0500

    Se termina toda la estructura de las pantallas login y register

--- app.js
+++ app.js
@@ -18,4 +18,8 @@ app.get('/login', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
 
+app.get('/register' , (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
 app.listen(port, () => console.log('Corriendo servidor'))
