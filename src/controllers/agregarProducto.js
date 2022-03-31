378020c5bb8d2174eb9a2dd20a8b00d3502f1fea
davidnope <dnope745@gmail.com>
2022-03-30 21:50:58 -0500

    Se termina pantalla agregar-producto

--- src/controllers/agregarProducto.js
+++ src/controllers/agregarProducto.js
@@ -1,9 +1,16 @@
 const path = require('path');
 
+const partesFormulario = {
+    marca: ['PlayStation', 'Xbox', 'Nintendo'],
+    categoria: ['Controles', 'Consolas', 'Accesorios'],
+    metodosPago: ['PSE', 'VISA','MASTERCARD', 'AMERICAN EXPRESS', 'DAVIVIENDA', 'BANCOLOMBIA'],
+    color: ['Negro', 'Blanco', 'Rojo', 'Azul', 'Gris', 'Otro']
+}   
+
 const controller = {
     agregarProducto : (req, res) =>{
         
-        res.render(path.resolve(__dirname, '../views/agregar-producto.ejs'))
+        res.render(path.resolve(__dirname, '../views/agregar-producto.ejs'), {partesFormulario})
     },
 }
 
