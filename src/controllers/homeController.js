83a70a9878e049ecf130ae1b726de6fb7f55d34a
David Nope <dnope745@gmail.com>
2022-04-21 21:50:06 -0500

    se arregla carrusel home

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,14 @@
-const  path  = require("path");
+const fs = require('fs');
+const path = require('path');
 
-
+const productsFilePath = path.join(__dirname, '../data/dbProductos.json');
+const productosJSON = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller={
     home: (req, res) => {
-        res.render(path.resolve(__dirname, '../views/home.ejs'))
+    
+        res.render(path.resolve(__dirname, '../views/home.ejs'), {productosJSON, toThousand})
     },
 }
 module.exports = controller;
